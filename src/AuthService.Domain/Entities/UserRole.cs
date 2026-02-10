1fe3687fd2312789ba3ed15d3a0e373b65a98666
TamatMiguel*18 <mtamat-2024024@kinal.edu.gt>
2026-02-10 08:59:51 -0600

    Se agrego una linea de comentario

--- src/AuthService.Domain/Entities/UserRole.cs
+++ src/AuthService.Domain/Entities/UserRole.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema; // Agrega esta línea para usar la anotación [ForeignKey]
 
 namespace AuthService.Domain.Entities;
 public class UserRole
