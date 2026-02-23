a4be8599ac2cb1ec286e0dc4b1152b551944eb1f
TamatMiguel*18 <mtamat-2024024@kinal.edu.gt>
2026-02-23 11:08:13 -0600

    Se crea la migracion de la base de datos

--- src/AuthService.Api/Program.cs
+++ src/AuthService.Api/Program.cs
@@ -1,9 +1,15 @@
+using AuthService.Api.Extensions;
+using AuthService.Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// CONFIGURACION DE SERVICIOS POR MEDIO DE METODOS DE EXTENSION
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -35,6 +41,26 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+// INICIALIZACION DE LA BASE DE DATOS
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    try{
+        logger.LogInformation("Iniciando migracion de la base de datos...");
+        
+        await context.Database.EnsureCreatedAsync(); // Crea la base de datos si no existe
+
+        logger.LogInformation("Migracion completada exitosamente.");
+        await DataSeeder.SeedAsync(context); // Seed de datos iniciales
+        logger.LogInformation("Seed de datos compleatada exitosamente.");
+    } catch(Exception ex){
+        logger.LogError(ex, "Error al inicializar la base de datos");
+        throw; // Detener la aplicación si la inicialización falla
+    }
+}
 
 app.Run();
 
